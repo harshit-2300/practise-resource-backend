b9e9997282d94945baf845ba1dd42a06bb74dbdc
harshit-2300 <srivastavaharshit333@gmail.com>
2021-07-15 15:03:03 +0530

    en

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app=express();
 var cors = require('cors')
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 const uri = process.env.DATABASE_URL;
